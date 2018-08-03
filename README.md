35e78fa482eefcfaad4957caa63546822acacc80
vicfeel <zhangwp0537@126.com>
2018-08-03 19:52:32 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # react-bucket
-react+redux+typescript+es6+antd+webpack
+
+* 基础框架：`react`
+
+* 状态管理：`redux`
+
+* 开发语言： `typescript`, `ES6`, `less`
+
+* 代码检查： `eslint`
+
+* 工程化构建：`webpack`
+
+* 路由：`react-router`
+
+* UI： `antd`
+
+* ajax： todo
--- src/common/selectors/breadcrumb.ts
+++ src/common/selectors/breadcrumb.ts
@@ -3,7 +3,6 @@ import {matchPath} from 'react-router';
 
 export const getCurrentBreadcrumb = (pathname: string, breadcrumbs: Breadcrumbs) => {
     return breadcrumbs.find(({path, exact}) => {
-        debugger;
         const match = matchPath(pathname, {path, exact});
 
         return !!match;
